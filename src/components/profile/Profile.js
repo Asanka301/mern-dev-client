f70c3e96b83eee4b827b2a1140107c825d15b2c1
Asanka301 <Asanka301@users.noreply.github.com>
2024-03-14 11:12:14 +0530

    add server

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,14 +1,14 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
-import { connect } from "react-redux";
-import Spinner from "../layout/Spinner";
-import ProfileTop from "./ProfileTop";
-import ProfileAbout from "./ProfileAbout";
-import ProfileExperience from "./ProfileExperience";
-import ProfileEducation from "./ProfileEducation";
-import ProfileGithub from "./ProfileGithub";
-import { getProfileById } from "../../actions/profile";
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link, useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
+import { getProfileById } from '../../actions/profile';
 
 const Profile = ({ getProfileById, profile: { profile }, auth }) => {
   const { id } = useParams();
@@ -80,12 +80,12 @@ const Profile = ({ getProfileById, profile: { profile }, auth }) => {
 Profile.propTypes = {
   getProfileById: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
   profile: state.profile,
-  auth: state.auth,
+  auth: state.auth
 });
 
 export default connect(mapStateToProps, { getProfileById })(Profile);
